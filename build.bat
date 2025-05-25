f6a1cb560b2d921d0908c5240478ad72644384f7
Douglas S. Elias <douglassantoselias@gmail.com>
2025-05-25 17:14:50 -0300

    Remove main file

--- build.bat
+++ build.bat
@@ -6,7 +6,6 @@ where /q cl || call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTo
 rmdir /s /q build
 mkdir build
 
-@REM set filename=main.cpp
 @REM set filename=001_colored_background.cpp
 @REM set filename=002_single_color_triangle.cpp
 set filename=003_multi_colored_triangle.cpp
