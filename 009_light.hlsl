181b7bd5fbc83240741997a8733a318d0be3b49d
Douglas S. Elias <douglassantoselias@gmail.com>
2026-02-16 10:13:29 -0300

    Update code

--- 009_light.hlsl
+++ 009_light.hlsl
@@ -67,25 +67,19 @@ float4 ps_main(Pixel p) : SV_TARGET
   float3 lightColor = float3(1, 1, 1);
   float3 lightDirection = float3(0, 0, 0);
   
-  // Directional light (mesma direção para todos os pixels, como sol):
   lightDirection = normalize(float3(0.1, 0.2, -0.4));
-  // Point light (direção depende da posição do fragmento):
-  // lightDirection = normalize(light_position - p.frag_pos);
+
   float ambientStrength = 0.1;
   float3 ambient = ambientStrength * lightColor * tex_sample.rgb;
   float dot_light = dot(p.normal, lightDirection);
   float diffuseStrength = saturate(dot_light);
   float3 diffuse = diffuseStrength * lightColor * tex_sample.rgb;
-  // return float4(ambient + diffuse, 1);
-  /// Specular (Phong)
+
   float shininess = 32;
   float3 viewDir = normalize(camera_position - p.frag_pos);
   float3 reflectionVector = normalize(reflection(p.normal, lightDirection));
   float spec = pow(saturate(dot(reflectionVector, viewDir)), shininess);
   float3 specular = spec * lightColor;
-  // return float4(p.normal * 0.5 + 0.5, 1);  // visualizar normais
-  // return float4(diffuse, 1);                 // só diffuse
-  // return float4(specular, 1);                // só specular
   float4 result = float4(ambient + diffuse + specular, 1);
   return result;
 }
